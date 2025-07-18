d94c31bff71344d3a820aeb1e20a7c04485f7e84
Lachezar Lechev <lachezar@ambire.com>
2025-07-18 12:08:48 +0300

    fix: Player - selected check before video.load
    Signed-off-by: Lachezar Lechev <lachezar@ambire.com>

--- src/routes/Player/Player.js
+++ src/routes/Player/Player.js
@@ -323,7 +323,8 @@ const Player = ({ urlParams, queryParams }) => {
         setError(null);
         video.unload();
 
-        if (player.stream?.type === 'Ready' && streamingServer.settings?.type !== 'Loading') {
+        // if (player.stream?.type === 'Ready' && streamingServer.settings?.type !== 'Loading') {
+        if (player.selected && player.stream?.type === 'Ready' && streamingServer.settings?.type !== 'Loading') {
             video.load({
                 stream: {
                     ...player.stream.content,
@@ -361,7 +362,7 @@ const Player = ({ urlParams, queryParams }) => {
                 shellTransport: services.shell.active ? services.shell.transport : null,
             });
         }
-    }, [streamingServer.baseUrl, player.selected, forceTranscoding, casting]);
+    }, [streamingServer.baseUrl, player.selected, player.selected, forceTranscoding, casting]);
     React.useEffect(() => {
         if (video.state.stream !== null) {
             const tracks = player.subtitles.map((subtitles) => ({
